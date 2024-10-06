a5651928c01c3d0888835b5220305006595b0a3c
Holger-Mayer <60851489+Holger-Mayer@users.noreply.github.com>
2024-10-06 20:44:40 +0200

    initial version

---
+++ src/main/java/de/h_mayer/testmanager/TestmanagerApplication.java
@@ -0,0 +1,13 @@
+package de.h_mayer.testmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/h_mayer/testmanager/TestmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.h_mayer.testmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
